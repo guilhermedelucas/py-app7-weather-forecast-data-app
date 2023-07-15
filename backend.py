a33638d20adb0a135316bcac24e3d267f5b7ba39
guilhermedelucas <gdelucasde@gmail.com>
2023-07-15 12:15:59 +0200

    Added error handling if place it's not found.

--- backend.py
+++ backend.py
@@ -2,14 +2,16 @@ import requests
 import config
 
 def get_data(place, forecast_days=None):
-    url = f'https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={config.API_KEY}'
-    response = requests.request('get', url)
-    data = response.json()
-    filtered_data = data['list']
-    nr_values = 8 * forecast_days
-    filtered_data = filtered_data[:nr_values]
-    return filtered_data
-
+    try:
+        url = f'https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={config.API_KEY}'
+        response = requests.request('get', url)
+        data = response.json()
+        filtered_data = data['list']
+        nr_values = 8 * forecast_days
+        filtered_data = filtered_data[:nr_values]
+        return filtered_data
+    except KeyError:
+        return None
 
 if __name__ == '__main__':
     get_data('Berlin', 3)
--- main.py
+++ main.py
@@ -10,29 +10,31 @@ option = st.selectbox('Select data to view:',
                       ('Temperature', 'Sky'))
 
 if place:
-    st.subheader(f'{option} for the next {days} in {place}')
     data = get_data(place=place, forecast_days=days)
+    if data:
+        st.subheader(f'{option} for the next {days} in {place}')
+        if option == 'Temperature':
+            # Create a temperature plot
+            temperatures = [i['main']['temp'] / 10 for i in data]
+            dates = [i['dt_txt'] for i in data]
+            # Create a temperature plot
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature"})
+            st.plotly_chart(figure)
 
-    if option == 'Temperature':
-        # Create a temperature plot
-        temperatures = [i['main']['temp'] / 10 for i in data]
-        dates = [i['dt_txt'] for i in data]
-        # Create a temperature plot
-        figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature"})
-        st.plotly_chart(figure)
+        if option == 'Sky':
+            sky_conditions = [i['weather'][0]['main'] for i in data]
 
-    if option == 'Sky':
-        sky_conditions = [i['weather'][0]['main'] for i in data]
+            images = {
+                'Clear': 'images/clear.png',
+                'Clouds': 'images/cloud.png',
+                'Rain': 'images/rain.png',
+                'Snow': 'images/snow.png',
+            }
 
-        images = {
-            'Clear': 'images/clear.png',
-            'Clouds': 'images/cloud.png',
-            'Rain': 'images/rain.png',
-            'Snow': 'images/snow.png',
-        }
-
-        image_paths = [images[conditions] for conditions in sky_conditions]
-        dates = [i['dt_txt'] for i in data]
-        print(dates)
-        # Render sky images
-        st.image(image_paths, caption=dates, width=115)
+            image_paths = [images[conditions] for conditions in sky_conditions]
+            dates = [i['dt_txt'] for i in data]
+            print(dates)
+            # Render sky images
+            st.image(image_paths, caption=dates, width=115)
+    else:
+        st.error('Invalid place')
